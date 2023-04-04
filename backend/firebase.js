cad7262e097fcbcb2ef530d744d47c0f1335c26e
ritakura <reina.itk@gmail.com>
2023-04-04 11:04:27 -0700

    this is so scuffed, time to read docs

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,33 +1,40 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 
-import { getFirestore } from "firebase/firestore";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+// import { getFirestore } from "firebase/firestore";
+// import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
 
 module.exports = function(app){
-    // TODO: Add SDKs for Firebase products that you want to use
-    // https://firebase.google.com/docs/web/setup#available-libraries
+    const firebase = require("firebase/app");
 
-    // Your web app's Firebase configuration
-    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    require("firebase/auth");
+    require("firebase/firestore");
+    // storage
+
+    var admin = require("firebase-admin");
+    var serviceAccount = require("./sharedhouse-yosemite-firebase-adminsdk-5ln2b-5f618a51cb.json");
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount)
+    });
+
     const firebaseConfig = {
-    apiKey: "AIzaSyDT-MnGUOVt_DH6qLonDqZYTmTs8dIm6Sc",
-    authDomain: "sharedhouse-yosemite.firebaseapp.com",
-    projectId: "sharedhouse-yosemite",
-    storageBucket: "sharedhouse-yosemite.appspot.com",
-    messagingSenderId: "913316799799",
-    appId: "1:913316799799:web:560e1c5ea73341a2dce6b2",
-    measurementId: "G-8S2H8126XZ"
+        apiKey: "AIzaSyDT-MnGUOVt_DH6qLonDqZYTmTs8dIm6Sc",
+        authDomain: "sharedhouse-yosemite.firebaseapp.com",
+        projectId: "sharedhouse-yosemite",
+        storageBucket: "sharedhouse-yosemite.appspot.com",
+        messagingSenderId: "913316799799",
+        appId: "1:913316799799:web:560e1c5ea73341a2dce6b2",
+        measurementId: "G-8S2H8126XZ"
     };
 
     // Initialize Firebase
-    const fb_app = initializeApp(firebaseConfig);
-    const analytics = getAnalytics(fb_app);
+    const fb_app = firebase.initializeApp(firebaseConfig);
+    // const analytics = fb_app.analytics();
 
-    const auth = getAuth();
+    const auth = admin.auth();
 
-    let db = getFirestore(fb_app);
+    let db = admin.firestore();
 
         app.post('/api', (req, res) => {
             const text = req.body.name
