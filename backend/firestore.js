f72f697a50c804deb7ccb7325cc0198f039762a6
ritakura <reina.itk@gmail.com>
2023-04-11 10:30:14 -0700

    changed firestore.js

--- backend/firestore.js
+++ backend/firestore.js
@@ -3,16 +3,19 @@ module.exports = function(app, admin){
 
     let db = admin.firestore();
 
-    app.post('/api', (req, res) => {
-        const text = req.body.name
-        res.send({ message: text })
-        db.collection("names").add({
-            name: text
-        }).then((docRef) => {
-            console.log("Docment written with ID: ", docRef.id);
-        }).catch((error) => {
-            console.error("error adding document: ", error);
-        })
+    app.post('/firestore', (req, res) => {
+        const type = req.body.type
+        const data = req.body.data
+        res.send({ message: type })
+        if (type == 'listing') {
+            db.collection(type).add({
+                name: data.title
+            }).then((docRef) => {
+                console.log("Docment written with ID: ", docRef.id);
+            }).catch((error) => {
+                console.error("error adding document: ", error);
+            })
+        }
     })
 
 }
