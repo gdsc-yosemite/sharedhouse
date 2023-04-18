5271976d6f41f919c72ff6b5f87f9b79ec15e05b
Andrew Fojas <andrewfojas@campus-037-176.ucdavis.edu>
2023-04-17 19:16:35 -0700

    Added css styling to listings

--- frontend/src/pages/Listing.js
+++ frontend/src/pages/Listing.js
@@ -1,6 +1,7 @@
 
 import React, { useState } from "react";
 import '/Users/macbookairm1/Documents/CS Projects/Shared-house-proj/sharedhouse/frontend/src/App.css';
+import './Listings.css';
 
 function Listing() {
   const [inputs, setInputs] = useState({
@@ -32,60 +33,70 @@ function Listing() {
             name="listing_title"
             value={inputs.listing_title}
             onChange={handleInputChange}
+            className="input-text"
           />
           <input
             type="text"
             name="address"
             value={inputs.address}
             onChange={handleInputChange}
+            className="input-text"
           />
           <input
             type="text"
             name="property_SqFt"
             value={inputs.property_SqFt}
             onChange={handleInputChange}
+            className="input-text"
           />
           <input
             type="text"
             name="lease_start_date"
             value={inputs.lease_start_date}
             onChange={handleInputChange}
+            className="input-text"
           />
           <input
             type="text"
             name="location"
             value={inputs.location}
             onChange={handleInputChange}
+            className="input-text"
           />
           <input
             type="text"
             name="listing_price"
             value={inputs.listing_price}
             onChange={handleInputChange}
+            className="input-text"
           />
           <input
             type="text"
             name="lease_end_date"
             value={inputs.lease_end_date}
             onChange={handleInputChange}
+            className="input-text"
           />
           <input
             type="text"
             name="lease_rate"
             value={inputs.lease_rate}
             onChange={handleInputChange}
+            className="input-text"
           />
           <input
             type="text"
             name="description"
             value={inputs.description}
             onChange={handleInputChange}
+            className="input-text"
           />
           <input
             type="text"
             name="contact_info"
             value={inputs.contact_info}
             onChange={handleInputChange}
+            className="input-text"
           />
         </div>
         
