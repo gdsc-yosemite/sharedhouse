e9822738438b37a0f24ed130b9a9de91fd5c02e7
ritakura <reina.itk@gmail.com>
2023-06-01 23:19:15 -0700

    fixing styling

--- frontend/src/pages/MyListings.js
+++ frontend/src/pages/MyListings.js
@@ -79,7 +79,11 @@ function MyListings() {
                         <div className="listing-address-info listing-info">
                             <div className="listing-label">Address:</div>
                             <div className="listing-address">
-                                <div className="listing-address-1">{listing.address}, RM {listing.room_appt_num}</div>
+                                <div className="listing-address-1">{listing.address} 
+                                    { listing.room_appt_num != null && (
+                                        <div>, RM {listing.room_appt_num}</div>
+                                    )}
+                                </div>
                                 <div className="listing-address-2">{listing.city}, {listing.state} {listing.zip}</div>
                             </div>
                         </div>
